1f555ec92e4afbeeda1bfb4cced4647460d64c75
IInnffiinniittyy <tanuj813@gmail.com>
2022-12-17 00:50:18 +0530

    submit

--- SatyaBusReservationSystem/src/main/java/com/masai/repository/AdminDao.java
+++ SatyaBusReservationSystem/src/main/java/com/masai/repository/AdminDao.java
@@ -1,9 +1,10 @@
 package com.masai.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.masai.model.Admin;
-
+@Repository
 public interface AdminDao extends JpaRepository<Admin, Integer> {
 
 	public Admin findByMobileNumber(String mobileNumber);
